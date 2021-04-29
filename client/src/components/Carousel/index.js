1e442683f8972864a0a0b22fad81509eac65b8cd
Mike M <mikal815@users.noreply.github.com>
2021-04-28 21:41:25 -0400

    Autoplay carousel

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -9,6 +9,7 @@ export default function SimpleSlider() {
     var settings = {
         dots: true,
         infinite: true,
+        autoplay: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
